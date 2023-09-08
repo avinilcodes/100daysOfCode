a9355304920fbb18d8682064d68348ffc29c3546
Unlearn N Learn <avinil.bedarkar10@gmail.com>
2023-09-08 16:57:14 +0530

    Day 15

---
+++ to-lower-case.py
@@ -0,0 +1,15 @@
+
+
+# Runtime
+# 7ms
+# Beats 96.65%of users with Python
+# Memory
+# 13.20MB
+# Beats 95.67%of users with Python
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.lower()
